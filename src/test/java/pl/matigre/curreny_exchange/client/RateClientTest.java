8d978af670cfe84612d5644060516cc5a30fc3e0
pavlo <ggasiorowski@gmail.com>
2021-05-10 22:07:19 +0200

    refactoring klienta i testu

--- src/test/java/pl/matigre/curreny_exchange/client/RateClientTest.java
+++ src/test/java/pl/matigre/curreny_exchange/client/RateClientTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.matigre.curreny_exchange.client.dto.RateMainDto;
+import pl.matigre.curreny_exchange.mapper.RateDtoToModelMapper;
 import pl.matigre.curreny_exchange.model.Rate;
 import pl.matigre.curreny_exchange.model.RateMain;
 
@@ -13,13 +15,17 @@ import java.time.LocalDate;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RateClientTest {
 
-    @Mock
+    @InjectMocks
     private RateClient rateClient;
+
+    @Mock
+    private RateDtoToModelMapper rateDtoToModelMapper;
 
     @Test
     void should_getRateForObject_return_RateMain() {
@@ -27,7 +33,7 @@ class RateClientTest {
         final RateMain expected = rateMain();
         final String table = "a";
         final String code = "eur";
-        when(rateClient.getRateForObject(table, code)).thenReturn(expected);
+        when(rateDtoToModelMapper.mapRateMainDtoToRateMain(any(RateMainDto.class))).thenReturn(expected);
 
         //when
         RateMain result = rateClient.getRateForObject(table, code);
@@ -40,16 +46,16 @@ class RateClientTest {
         return RateMain.builder()
                 .table("a")
                 .currency("eur")
-                .code("dolar amerykański")
+                .code("eur")
                 .rates(asList(rate()))
                 .build();
     }
 
     private Rate rate() {
-        BigDecimal bigDecimal = BigDecimal.valueOf(13234.65);
-        LocalDate date = LocalDate.of(2000, 5, 12);
+        BigDecimal bigDecimal = BigDecimal.valueOf(4.5631);
+        LocalDate date = LocalDate.of(2021, 5, 10);
         return Rate.builder()
-                .no("213s")
+                .no("088/A/NBP/2021")
                 .mid(bigDecimal)
                 .effectiveDate(date)
                 .build();
