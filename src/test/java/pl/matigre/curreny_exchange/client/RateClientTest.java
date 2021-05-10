e421211279759fde7cda023c2201980689245603
Mateusz Rucinski <m.rucinski15@gmail.com>
2021-05-10 20:25:50 +0200

    Change 05 - TestRateClient

--- src/test/java/pl/matigre/curreny_exchange/client/RateClientTest.java
+++ src/test/java/pl/matigre/curreny_exchange/client/RateClientTest.java
@@ -1,21 +1,58 @@
 package pl.matigre.curreny_exchange.client;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.matigre.curreny_exchange.model.Rate;
+import pl.matigre.curreny_exchange.model.RateMain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class RateClientTest {
 
+    @Mock
+    private RateClient rateClient;
+
     @Test
-    void should_getRateForObject_is_null() {
+    void should_getRateForObject_return_RateMain() {
         //given
+        final RateMain expected = rateMain();
+        final String table = "a";
+        final String code = "eur";
+        when(rateClient.getRateForObject(table, code)).thenReturn(expected);
 
         //when
+        RateMain result = rateClient.getRateForObject(table, code);
 
         //then
+        assertThat(result).isEqualTo(expected);
     }
 
-    void should_() {
+    private RateMain rateMain() {
+        return RateMain.builder()
+                .table("a")
+                .currency("eur")
+                .code("dolar amerykański")
+                .rates(asList(rate()))
+                .build();
+    }
 
+    private Rate rate() {
+        BigDecimal bigDecimal = BigDecimal.valueOf(13234.65);
+        LocalDate date = LocalDate.of(2000, 5, 12);
+        return Rate.builder()
+                .no("213s")
+                .mid(bigDecimal)
+                .effectiveDate(date)
+                .build();
     }
 
 }
