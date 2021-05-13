280dea3b453331a345daf841cb725bf8a7d378c5
Mateusz Rucinski <m.rucinski15@gmail.com>
2021-05-13 21:04:20 +0200

    Change 06 - TestController Integration Testing JUnit 5

--- src/test/java/pl/matigre/curreny_exchange/controller/RateControllerTest.java
+++ src/test/java/pl/matigre/curreny_exchange/controller/RateControllerTest.java
@@ -1,10 +1,19 @@
 package pl.matigre.curreny_exchange.controller;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pl.matigre.curreny_exchange.model.Rate;
 import pl.matigre.curreny_exchange.model.RateMain;
 import pl.matigre.curreny_exchange.service.RateService;
@@ -14,38 +23,45 @@ import java.time.LocalDate;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //for RestTemplate
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 class RateControllerTest {
 
-    @InjectMocks
-    private RateController rateController;
+    @Autowired
+    TestRestTemplate testRestTemplate;
 
-    @Mock
-    private RateService rateService;
+    @MockBean
+    private RateService service;
 
     @Test
-    void should_getRate_getRateService() {
+    void should_getRate() throws JSONException {
         //given
-        RateMain expected = rateMain();
-        final String table = "a";
-        final String code = "eur";
-        when(rateService.getRate(table, code)).thenReturn(expected);
+        final RateMain rateMain = rateMain();
+        when(service.getRate("a", "eur")).thenReturn(rateMain);
+        final String expected = "{\"table\":\"a\",\"currency\":\"eur\",\"code\":\"1\",\"rates\":[{\"no\":null,\"effectiveDate\":null,\"mid\":null}]}";
         //when
-        RateMain result = rateController.getRate(table, code);
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/rate/a/eur", String.class);
 
         //then
-        assertThat(result).isEqualTo(expected);
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+        JSONAssert.assertEquals(expected, response.getBody(), false);
 
+        verify(service, times(1)).getRate("a", "eur"); // parametr times nie jest potrzebny jesli wartość równa się 1 to jest domyślna wartość
     }
 
     private RateMain rateMain() {
         return RateMain.builder()
                 .table("a")
                 .currency("eur")
-                .code("eur")
-                .rates(asList(rate()))
+                .code("1")
+                .rates(asList(Rate.builder().build()))
                 .build();
     }
 
